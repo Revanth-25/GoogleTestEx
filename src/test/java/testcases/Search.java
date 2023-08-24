2e5b70df61eb889e7fbf05dcce3f935ffd37f2ae
Revanth-25 <142740859+Revanth-25@users.noreply.github.com>
2023-08-24 16:14:43 +0530

    Update Search.java

--- src/test/java/testcases/Search.java
+++ src/test/java/testcases/Search.java
@@ -9,6 +9,8 @@ public class Search extends baseTest{
 	@Test
 	public static void testButton() throws Exception
 	{
+	
+		Thread.sleep(3000);
 		
 		driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[6]/center/input[1]")).click();
 		
